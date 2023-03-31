bcf3c48148ee5df5ac0b602c3d88b51693c7aad5
dzakyy04 <dzakylinggau@gmail>
2023-03-31 10:57:19 +0700

    mengganti data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,56 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password'),
             'is_admin' => true,
         ]);
+
+        Category::create([
+            'name' => 'Politik',
+            'slug' => 'politik',
+            'image' => 'https: //source.unsplash.com/700x300?political',
+            'background' => '#012970',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Olahraga',
+            'slug' => 'olahraga',
+            'image' => 'https: //source.unsplash.com/700x300?sport',
+            'background' => '#FF914D',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Bisnis',
+            'slug' => 'bisnis',
+            'image' => 'https://source.unsplash.com/700x300?business',
+            'background' => '#F7DC6F',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Teknologi',
+            'slug' => 'teknologi',
+            'image' => 'https://source.unsplash.com/700x300?technology',
+            'background' => '#95A5A6',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Pendidikan',
+            'slug' => 'pendidikan',
+            'image' => 'https://source.unsplash.com/700x300?education',
+            'background' => '#5DADE2',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Seni dan Budaya',
+            'slug' => 'seni-dan-budaya',
+            'image' => 'https://source.unsplash.com/700x300?art-culture',
+            'background' => '#F5B7B1',
+            'color' => '#fff',
+        ]);
+
         User::factory(5)->create();
-        Category::factory(50)->create();
         Article::factory(50)->create();
 
         // \App\Models\User::factory()->create([
