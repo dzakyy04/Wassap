ff2ab98b8dcdb9e2649c1480ea72cd50c3415bf7
dzakyy04 <dzakylinggau@gmail.com>
2023-04-18 20:14:25 +0700

    merubah seeder category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,50 +26,91 @@ class DatabaseSeeder extends Seeder
         Category::create([
             'name' => 'Politik',
             'slug' => 'politik',
-            'image' => 'https: //source.unsplash.com/700x300?political',
+            'image' => 'https://source.unsplash.com/700x300?political',
             'background' => '#012970',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Ekonomi',
+            'slug' => 'ekonomi',
+            'image' => 'https://source.unsplash.com/700x300?economy',
+            'background' => '#8e44ad',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Geografis',
+            'slug' => 'geografis',
+            'image' => 'https://source.unsplash.com/700x300?geography',
+            'background' => '#2ecc71',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Terpopuler',
+            'slug' => 'terpopuler',
+            'image' => 'https://source.unsplash.com/700x300?popular',
+            'background' => '#f1c40f',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Tren',
+            'slug' => 'tren',
+            'image' => 'https://source.unsplash.com/700x300?trending',
+            'background' => '#3498db',
             'color' => '#fff',
         ]);
 
         Category::create([
             'name' => 'Olahraga',
             'slug' => 'olahraga',
-            'image' => 'https: //source.unsplash.com/700x300?sport',
-            'background' => '#FF914D',
+            'image' => 'https://source.unsplash.com/700x300?sports',
+            'background' => '#e67e22',
             'color' => '#fff',
         ]);
 
         Category::create([
-            'name' => 'Bisnis',
-            'slug' => 'bisnis',
-            'image' => 'https://source.unsplash.com/700x300?business',
-            'background' => '#F7DC6F',
+            'name' => 'Kesehatan',
+            'slug' => 'kesehatan',
+            'image' => 'https://source.unsplash.com/700x300?health',
+            'background' => '#c0392b',
             'color' => '#fff',
         ]);
 
         Category::create([
-            'name' => 'Teknologi',
-            'slug' => 'teknologi',
+            'name' => 'Gaya Hidup',
+            'slug' => 'gaya-hidup',
+            'image' => 'https://source.unsplash.com/700x300?lifestyle',
+            'background' => '#27ae60',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Entertainment',
+            'slug' => 'entertainment',
+            'image' => 'https://source.unsplash.com/700x300?entertainment',
+            'background' => '#9b59b6',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'Kejahatan',
+            'slug' => 'kejahatan',
+            'image' => 'https://source.unsplash.com/700x300?crime',
+            'background' => '#e74c3c',
+            'color' => '#fff',
+        ]);
+
+        Category::create([
+            'name' => 'IPTEK',
+            'slug' => 'iptek',
             'image' => 'https://source.unsplash.com/700x300?technology',
-            'background' => '#95A5A6',
+            'background' => '#1abc9c',
             'color' => '#fff',
         ]);
 
-        Category::create([
-            'name' => 'Pendidikan',
-            'slug' => 'pendidikan',
-            'image' => 'https://source.unsplash.com/700x300?education',
-            'background' => '#5DADE2',
-            'color' => '#fff',
-        ]);
-
-        Category::create([
-            'name' => 'Seni dan Budaya',
-            'slug' => 'seni-dan-budaya',
-            'image' => 'https://source.unsplash.com/700x300?art-culture',
-            'background' => '#F5B7B1',
-            'color' => '#fff',
-        ]);
 
         User::factory(5)->create();
         Article::factory(50)->create();
